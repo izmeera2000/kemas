5abefc706639a8d1c0b1eb7da4b76f22dc2782f6
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2023-12-16 23:05:21 +0800

    asdas
    dasdasdas

--- functions.php
+++ functions.php
@@ -588,9 +588,9 @@ if (isset($_POST['download-pdf-borang'])) {
 
 
 
-    $tablebahA3 = new easyTable($pdf, '%{5,33,33,29}', 'border:0;font-size:8;');
-    $tablebahA3->easyCell('<b>Orang yang boleh dihubungi jika berlaku kecemasan:</b>', 'valign:L;  align:L;colspan:3');
-    $tablebahA3->printRow();
+    // $tablebahA3 = new easyTable($pdf, '%{5,33,33,29}', 'border:0;font-size:8;');
+    // $tablebahA3->easyCell('<b>Orang yang boleh dihubungi jika berlaku kecemasan:</b>', 'valign:L;  align:L;colspan:3');
+    // $tablebahA3->printRow();
 
     // $tablebahA3->easyCell('asd');
     // $tablebahA3->easyCell('Nama : ', 'valign:L;  align:L;paddingY:2');
@@ -612,7 +612,7 @@ if (isset($_POST['download-pdf-borang'])) {
     // $tablebahA3->easyCell($hubungan_penjaga, 'valign:L;  align:L;paddingY:2');
     // $tablebahA3->printRow();
 
-    $tablebahA3->endTable(5);
+    // $tablebahA3->endTable(5);
 
     // $pdf->AddPage();
 
