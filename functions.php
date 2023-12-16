93d7f668aa8efc8708d1f9407967aeca9ad2c380
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2023-12-16 22:33:48 +0800

    asd
    asdasd

--- functions.php
+++ functions.php
@@ -572,7 +572,7 @@ if (isset($_POST['download-pdf-borang'])) {
     $tablebahA2->easyCell('Tinggi : ' . $tinggi . 'cm', 'valign:L;  align:L;paddingY:2');
     $tablebahA2->printRow();
 
-    $tablebahA2->easyCell('');
+    $tablebahA2->easyCell('asas');
     $tablebahA2->easyCell('Berat : ' . $berat . 'kg', 'valign:L;  align:L;paddingY:2');
     $tablebahA2->printRow();
 
