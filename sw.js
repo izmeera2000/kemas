c9688aada231cb24a4a57902b3ff3f9d521034aa
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2023-12-18 11:00:03 +0800

    asdasd
    asdasdasdasdas

--- sw.js
+++ sw.js
@@ -4,13 +4,13 @@ self.addEventListener("install", evt => self.skipWaiting());
 // (B) CLAIM CONTROL INSTANTLY
 self.addEventListener("activate", evt => self.clients.claim());
  
-// (C) LISTEN TO PUSH
-self.addEventListener("push", evt => {
-  const data = evt.data.json();
-  // console.log("Push", data);
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: data.icon,
-    image: data.image
-  });
-});
+// // (C) LISTEN TO PUSH
+// self.addEventListener("push", evt => {
+//   const data = evt.data.json();
+//   // console.log("Push", data);
+//   self.registration.showNotification(data.title, {
+//     body: data.body,
+//     icon: data.icon,
+//     image: data.image
+//   });
+// });
