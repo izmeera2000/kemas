48101da2f1ddf377697f7bb6b6348a58210d0359
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2023-12-18 11:07:04 +0800

    asdas
    dasdasdasdas

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@ self.addEventListener("activate", evt => self.clients.claim());
  
 // (C) LISTEN TO PUSH
 self.addEventListener("push", evt => {
+    console.log(evt.data);
   const data = evt.data.json();
-  console.log("Push", data);
   self.registration.showNotification(data.title, {
     body: data.body,
     icon: data.icon,
