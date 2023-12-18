2a32460ad662ec6e11d27006458ab441f4289775
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2023-12-18 10:53:54 +0800

    asdas
    dasdasdasdas

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,58 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+// (A) OBTAIN USER PERMISSION TO SHOW NOTIFICATION
+window.onload = () => {
+  // (A1) ASK FOR PERMISSION
+  if (Notification.permission === "default") {
+    Notification.requestPermission().then(perm => {
+      if (Notification.permission === "granted") {
+        regWorker().catch(err => console.error(err));
+      } else {
+        alert("Please allow notifications.");
+      }
+    });
+  } 
+ 
+  // (A2) GRANTED
+  else if (Notification.permission === "granted") {
+    regWorker().catch(err => console.error(err));
+  }
 
-serviceWorkerRegistration.pushManager.subscribe({
-  userVisibleOnly: true,
-  applicationServerKey: urlBase64ToUint8Array("BJF9s842CaIRdkrZ8Ds5eTktDmDR2GLEhXSQAmXQOmtt9V1T5zCpKfsY_csHYOpU4ksD35tevV9cwPfZdpslTXY")
-})
+  // (A3) DENIED
+  else { alert("Please allow notifications."); }
+}
+// (B) REGISTER SERVICE WORKER
+async function regWorker () {
+  // (B1) YOUR PUBLIC KEY - CHANGE TO YOUR OWN!
+  const publicKey = "BJF9s842CaIRdkrZ8Ds5eTktDmDR2GLEhXSQAmXQOmtt9V1T5zCpKfsY_csHYOpU4ksD35tevV9cwPfZdpslTXY";
+ 
+  // (B2) REGISTER SERVICE WORKER
+  navigator.serviceWorker.register("sw.js");
+ 
+  // (B3) SUBSCRIBE TO PUSH SERVER
+  navigator.serviceWorker.ready
+  .then(reg => {
+    reg.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: publicKey
+    }).then(
+      // (B3-1) OK - TEST PUSH NOTIFICATION
+      sub => {
+        var data = new FormData();
+        data.append("sub", JSON.stringify(sub));
+        fetch("5-push-server.php", { method: "POST", body : data })
+        .then(res => res.text())
+        .then(txt => console.log(txt))
+        .catch(err => console.error(err));
+      },
+ 
+      // (B3-2) ERROR!
+      err => console.error(err)
+    );
+  });
+}
+
 
 function previewImage(event) {
   var input = event.target;
