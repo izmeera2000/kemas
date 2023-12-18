41bfd62d32719771de71424c52121d32169590e4
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2023-12-18 11:55:35 +0800

    asdasdasdas
    dasdasdasd

--- kemas.sql
+++ kemas.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 27, 2023 at 03:52 PM
+-- Generation Time: Dec 18, 2023 at 03:55 AM
 -- Server version: 8.0.31
--- PHP Version: 8.0.26
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,31 +30,31 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `keluarga`;
 CREATE TABLE IF NOT EXISTS `keluarga` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `no_kad_pengenalan_murid` text,
-  `hubungan` text,
+  `no_kad_pengenalan_murid` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `hubungan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `kad_pengenalan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `nama` text,
+  `nama` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `tarikh_lahir` datetime DEFAULT NULL,
-  `tempat_lahir` text,
-  `warganegara` text,
-  `keturunan` text,
-  `pekerjaan` text,
-  `status` text,
-  `pendapatan_sebulan` text,
-  `no_telefon_pejabat` text,
-  `nama_majikan` text,
-  `alamat_majikan` text,
-  `file_slipgaji` text,
-  `file_pengesahan` text,
+  `tempat_lahir` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `warganegara` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `keturunan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `pekerjaan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `status` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `pendapatan_sebulan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `no_telefon_pejabat` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `nama_majikan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `alamat_majikan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `file_slipgaji` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `file_pengesahan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `keluarga`
 --
 
 INSERT INTO `keluarga` (`id`, `no_kad_pengenalan_murid`, `hubungan`, `kad_pengenalan`, `nama`, `tarikh_lahir`, `tempat_lahir`, `warganegara`, `keturunan`, `pekerjaan`, `status`, `pendapatan_sebulan`, `no_telefon_pejabat`, `nama_majikan`, `alamat_majikan`, `file_slipgaji`, `file_pengesahan`) VALUES
-(3, '000723140251', 'Bapa', '000723140242', 'asd', '2000-07-23 00:00:00', 'asd', 'asd', 'asd', 'asd', 'Kahwin', '1232', '12321', 'asd', 'asd', 'slipgaji.pdf', 'bahagianc.pdf');
+(8, '000723140151', 'Bapa', '000723140259', 'asd', '2000-07-23 00:00:00', 'asas', 'asd', 'asd', 'asd', 'Kahwin', '1232', '12321', 'asd', 'LOT 1214, LORONG PERTANIAN, KAMPUNG JAYA SETIA', 'slipgaji.pdf', 'bahagianc.pdf');
 
 -- --------------------------------------------------------
 
@@ -66,20 +66,53 @@ DROP TABLE IF EXISTS `keluarga_tanggungan`;
 CREATE TABLE IF NOT EXISTS `keluarga_tanggungan` (
   `id` int NOT NULL AUTO_INCREMENT,
   `no_kad_pengenalan_murid` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `nama` text,
-  `umur` text,
-  `perhubungan` text,
-  `nama_institusi` text,
-  `nilai_biasiswa` text,
+  `nama` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `umur` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `perhubungan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `nama_institusi` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `nilai_biasiswa` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `keluarga_tanggungan`
 --
 
 INSERT INTO `keluarga_tanggungan` (`id`, `no_kad_pengenalan_murid`, `nama`, `umur`, `perhubungan`, `nama_institusi`, `nilai_biasiswa`) VALUES
-(2, '000723140251', 'asd', 'sad', 'asd', 'asd', '12');
+(3, '000723140151', 'asd', '12', 'asd', 'asd', '121');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meeting`
+--
+
+DROP TABLE IF EXISTS `meeting`;
+CREATE TABLE IF NOT EXISTS `meeting` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nama` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `created_by` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `maklumat` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `tarikh_mula` timestamp NULL DEFAULT NULL,
+  `tarikh_akhir` timestamp NULL DEFAULT NULL,
+  `status` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meeting_kehadiran`
+--
+
+DROP TABLE IF EXISTS `meeting_kehadiran`;
+CREATE TABLE IF NOT EXISTS `meeting_kehadiran` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_murid` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `tarikh` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `nama_meeting` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -90,43 +123,52 @@ INSERT INTO `keluarga_tanggungan` (`id`, `no_kad_pengenalan_murid`, `nama`, `umu
 DROP TABLE IF EXISTS `murid`;
 CREATE TABLE IF NOT EXISTS `murid` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `name` text,
-  `age` text,
-  `no_kad_pengenalan` text,
+  `name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `email` text COLLATE utf8mb4_unicode_ci,
+  `age` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `no_kad_pengenalan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `tarikh_mula` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `warganegara` text,
-  `bangsa` text,
+  `warganegara` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `bangsa` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `tarikh_lahir` datetime DEFAULT NULL,
-  `no_sijil_lahir` text,
-  `tempat_lahir` text,
-  `jantina` text,
-  `alamat_rumah` text,
-  `saizbaju` text,
-  `penyakit` text,
-  `tinggi` text,
-  `berat` text,
-  `masalah_makanan` text,
-  `kecacatan` text,
-  `nama_penjaga` text,
-  `alamat_rumah_penjaga` text,
+  `no_sijil_lahir` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `tempat_lahir` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `jantina` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `alamat_rumah` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `saizbaju` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `penyakit` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `tinggi` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `berat` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `masalah_makanan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `kecacatan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `nama_penjaga` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `alamat_rumah_penjaga` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `telefon_penjaga` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `hubungan_penjaga` text,
-  `gambar` text,
-  `file_mykid` text,
-  `file_sijil` text,
-  `file_rekod_kesihatan` text,
-  `geran` text,
+  `hubungan_penjaga` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `gambar` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `file_mykid` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `file_sijil` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `file_rekod_kesihatan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `geran` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `status_kemasukan` int DEFAULT NULL,
   `status_kemasukan_text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `murid`
+-- Table structure for table `notification`
 --
 
-INSERT INTO `murid` (`id`, `name`, `age`, `no_kad_pengenalan`, `tarikh_mula`, `warganegara`, `bangsa`, `tarikh_lahir`, `no_sijil_lahir`, `tempat_lahir`, `jantina`, `alamat_rumah`, `saizbaju`, `penyakit`, `tinggi`, `berat`, `masalah_makanan`, `kecacatan`, `nama_penjaga`, `alamat_rumah_penjaga`, `telefon_penjaga`, `hubungan_penjaga`, `gambar`, `file_mykid`, `file_sijil`, `file_rekod_kesihatan`, `geran`, `status_kemasukan`, `status_kemasukan_text`) VALUES
-(2, 'asd', '23', '000723140251', '2023-11-21 13:28:54', 'asd', 'asd', '2000-07-23 00:00:00', 'asd', 'asd', 'asd', 'asd', 'S', 'Lelah', '23', '23', NULL, 'asd', 'asd', 'asd', 'asd', 'asd', 'gambar.jpg', 'mykid.pdf', 'sijillahir.pdf', 'kesihatan.pdf', '0', 1, 'd');
+DROP TABLE IF EXISTS `notification`;
+CREATE TABLE IF NOT EXISTS `notification` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` int DEFAULT NULL,
+  `title` int DEFAULT NULL,
+  `body` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -139,47 +181,40 @@ CREATE TABLE IF NOT EXISTS `pemarkahan` (
   `id` int NOT NULL AUTO_INCREMENT,
   `murid_id` int NOT NULL,
   `tarikh` datetime DEFAULT CURRENT_TIMESTAMP,
-  `idpengguna` text,
-  `bm1` text,
-  `bm2` text,
-  `bm3` text,
-  `bi1` text,
-  `bi2` text,
-  `bi3` text,
-  `pi1` text,
-  `pi2` text,
-  `pi3` text,
-  `pi4` text,
-  `pi5` text,
-  `pi6` text,
-  `keterampilan` text,
-  `perkem1` text,
-  `perkem2` text,
-  `perkem3` text,
-  `perkem4` text,
+  `idpengguna` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `bm1` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `bm2` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `bm3` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `bi1` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `bi2` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `bi3` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `pi1` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `pi2` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `pi3` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `pi4` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `pi5` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `pi6` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `keterampilan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `perkem1` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `perkem2` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `perkem3` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `perkem4` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `perkem5` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `perkem6` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `kreativ1` text,
-  `kreativ2` text,
-  `sainsawal1` text,
-  `sainsawal2` text,
-  `matematikawal1` text,
-  `matematikawal2` text,
-  `matematikawal3` text,
-  `matematikawal4` text,
-  `matematikawal5` text,
-  `matematikawal6` text,
-  `kemanusiaan` text,
-  `catatan` text,
+  `kreativ1` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `kreativ2` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `sainsawal1` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `sainsawal2` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `matematikawal1` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `matematikawal2` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `matematikawal3` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `matematikawal4` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `matematikawal5` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `matematikawal6` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `kemanusiaan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `catatan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `pemarkahan`
---
-
-INSERT INTO `pemarkahan` (`id`, `murid_id`, `tarikh`, `idpengguna`, `bm1`, `bm2`, `bm3`, `bi1`, `bi2`, `bi3`, `pi1`, `pi2`, `pi3`, `pi4`, `pi5`, `pi6`, `keterampilan`, `perkem1`, `perkem2`, `perkem3`, `perkem4`, `perkem5`, `perkem6`, `kreativ1`, `kreativ2`, `sainsawal1`, `sainsawal2`, `matematikawal1`, `matematikawal2`, `matematikawal3`, `matematikawal4`, `matematikawal5`, `matematikawal6`, `kemanusiaan`, `catatan`) VALUES
-(9, 2, '2023-11-27 22:50:11', '', 'asd', 'asd', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '');
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -190,20 +225,19 @@ INSERT INTO `pemarkahan` (`id`, `murid_id`, `tarikh`, `idpengguna`, `bm1`, `bm2`
 DROP TABLE IF EXISTS `staf`;
 CREATE TABLE IF NOT EXISTS `staf` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `idpengguna` text,
-  `katalaluan` text,
+  `idpengguna` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `katalaluan` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `level` int DEFAULT '2',
   `tarikh` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `staf`
 --
 
 INSERT INTO `staf` (`id`, `idpengguna`, `katalaluan`, `level`, `tarikh`) VALUES
-(4, 'asdasd', '7815696ecbf1c96e6894b779456d330e', 2, '2023-11-27 15:51:07'),
-(3, '123', '811584043b844704c9bb9a6e99dd05d3', 1, '2023-11-27 15:34:23');
+(4, 'asdasd', '7815696ecbf1c96e6894b779456d330e', 1, '2023-11-27 15:51:07');
 
 -- --------------------------------------------------------
 
@@ -214,12 +248,12 @@ INSERT INTO `staf` (`id`, `idpengguna`, `katalaluan`, `level`, `tarikh`) VALUES
 DROP TABLE IF EXISTS `staf_profile`;
 CREATE TABLE IF NOT EXISTS `staf_profile` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `idpengguna` text,
-  `nama` text,
+  `idpengguna` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `nama` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   `umur` int NOT NULL,
-  `gambar` text,
+  `gambar` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `staf_profile`
@@ -228,7 +262,23 @@ CREATE TABLE IF NOT EXISTS `staf_profile` (
 INSERT INTO `staf_profile` (`id`, `idpengguna`, `nama`, `umur`, `gambar`) VALUES
 (1, 'test', NULL, 0, NULL),
 (2, '123', '123', 123, NULL),
-(6, 'asdasd', 'asd', 12312, NULL);
+(6, 'asdasd', 'asdd', 12312, 'gambar.jpg'),
+(7, 'asd', 'asd', 23, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `yuran`
+--
+
+DROP TABLE IF EXISTS `yuran`;
+CREATE TABLE IF NOT EXISTS `yuran` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_murid` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+  `tarikh` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `nama_yuran` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
