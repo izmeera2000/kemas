779c9dd14d46898e7766ead15438cd92d8dbe3be
izmeera2000 <79130602+izmeera2000@users.noreply.github.com>
2023-12-20 14:31:55 +0800

    asd
    asdasdasdasdas

--- functions.php
+++ functions.php
@@ -909,7 +909,9 @@ if (isset($_POST['download-pdf-borang'])) {
 
     $pdf->AddPage();
 
-    $pdf->setSourceFile("assets/murid/000723140251/bahagianc.pdf");
+    // $pdf->setSourceFile("assets/murid/" . $ic . "/bahagianc.pdf");
+    $pdf->setSourceFile("assets/murid/" . $ic . "/bahagianc.pdf");
+
     $tplId = $pdf->importPage(1);
     // use the imported page and place it at point 10,10 with a width of 100 mm
     $pdf->useTemplate($tplId, 0, 0);
